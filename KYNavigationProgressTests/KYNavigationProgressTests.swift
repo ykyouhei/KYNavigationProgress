57aa11eb62bbf3b5537762c16213c30c56a4d0a7
ykyouhei <kyouhei.lab@gmail.com>
2015-12-29 17:13:37 +0900

    Initial Commit

---
+++ KYNavigationProgressTests/KYNavigationProgressTests.swift
@@ -0,0 +1,36 @@
+//
+//  KYNavigationProgressTests.swift
+//  KYNavigationProgressTests
+//
+//  Created by 山口　恭兵 on 2015/12/29.
+//  Copyright © 2015年 kyo__hei. All rights reserved.
+//
+
+import XCTest
+@testable import KYNavigationProgress
+
+class KYNavigationProgressTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
