e654449d63b986cea238bfa6c97f56972aa024b5
kyyamagu <kyyamagu@r.recruit.co.jp>
2015-12-29 17:13:37 +0900

    Initial Commit

---
+++ KYNavigationProgress/KYNavigationProgress.h
@@ -0,0 +1,19 @@
+//
+//  KYNavigationProgress.h
+//  KYNavigationProgress
+//
+//  Created by 山口　恭兵 on 2015/12/29.
+//  Copyright © 2015年 kyo__hei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KYNavigationProgress.
+FOUNDATION_EXPORT double KYNavigationProgressVersionNumber;
+
+//! Project version string for KYNavigationProgress.
+FOUNDATION_EXPORT const unsigned char KYNavigationProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KYNavigationProgress/PublicHeader.h>
+
+
